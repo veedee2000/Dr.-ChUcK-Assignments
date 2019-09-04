8759ca82c77799a82cb4554c7bc1dc7fd1547ac8
veedee2000 <43088920+veedee2000@users.noreply.github.com>
2019-09-05 01:55:54 +0530

    Add files via upload
    U will need BeautifulSoup to run these so install it via  ---  pip install bs4   --- command

---
+++ c3w4/c3w4p2.py
@@ -0,0 +1,26 @@
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+count = int(input())
+position = int(input())
+i = 1
+while i <= count :
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    tags = soup('a')
+    p = 0
+    for tag in tags:
+        p += 1
+        if p == position :
+            url = tag.get('href', None)
+            print(url)
+            break
+    if i == count :
+        print(tag.get_text())
+    i += 1
